82e31dcf0f14fbe96e66c5fea672edf720a216b9
SiposZoltan63 <siposz@kkszki.hu>
2024-10-25 12:59:11 +0200

    first

---
+++ 1025feladat/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1025feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string azonosito;
+
+            do
+            {
+                Console.WriteLine("Addja meg a személy azonosítója első 10 jegyét: ");
+                azonosito = Console.ReadLine();
+
+                if (azonosito.Length != 10)
+                {
+                    Console.WriteLine("Ez az azonosító érvénytelen.");
+                }
+                else if (azonosito[0] % 2 == 0)
+                {
+                    Console.WriteLine("A személy nő");
+                }
+                else
+                {
+                    Console.WriteLine("A személy férfi");
+                }
+
+            } while(azonosito.Length != 10);
+
+            Console.ReadKey();
+
+        }
+    }
+}
