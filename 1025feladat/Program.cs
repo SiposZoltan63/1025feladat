76b6a6ad7d166b0d41a42859c243b6dd8d41f944
SiposZoltan63 <113338728+SiposZoltan63@users.noreply.github.com>
2024-11-07 17:08:02 +0100

    Update

--- 1025feladat/Program.cs
+++ 1025feladat/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,10 @@ namespace _1025feladat
     {
         static void Main(string[] args)
         {
+
+            int jelenlegiEv = DateTime.Now.Year;
             string azonosito;
-            int szulsorszam;
+            string azonosito2;
 
             do
             {
@@ -33,11 +36,33 @@ namespace _1025feladat
 
             } while(azonosito.Length != 10);
 
-            szulsorszam = azonosito[7];
+            string szulevString = azonosito[3] + azonosito[4];
+            int szulevInt = Convert.ToInt32(szulevString);
+            int Kor = jelenlegiEv - szulevInt;
 
-            Console.Write("A személy születési sorszáma: "+szulsorszam);
+            Console.Write("A személy születési sorszáma: " + azonosito[7] + azonosito[8] + azonosito[9]);
+            Console.Write($"A személy a {Kor} születésnapját ünnepli ebben az évben");
+            do
+            {
+                Console.WriteLine("Addja meg a személy azonosítója első 10 jegyét: ");
+                azonosito2 = Console.ReadLine();
+
+                if (azonosito2.Length != 10)
+                {
+                    Console.WriteLine("Ez az azonosító érvénytelen.");
+                }
+                else if (azonosito2[0] % 2 == 0)
+                {
+                    Console.WriteLine("A személy nő");
+                }
+                else
+                {
+                    Console.WriteLine("A személy férfi");
+                }
+
+            } while (azonosito2.Length != 10);
+
             Console.ReadKey();
-
         }
     }
 }
